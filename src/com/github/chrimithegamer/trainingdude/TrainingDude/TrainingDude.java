0ad8596fab030cbd92243f4eea10bf6d6e0f1429
Christian Anderson <chrimithegamer@gmail.com>
2012-06-29 11:26:52 -0600

    Got the eclipse stuff setup!

---
+++ src/com/github/chrimithegamer/trainingdude/TrainingDude/TrainingDude.java
@@ -0,0 +1,7 @@
+package com.github.chrimithegamer.trainingdude.TrainingDude;
+
+public class TrainingDude {
+
+	// Welcome!
+	// First off, these slashes mean a 'comment', they won't interfere with the code in any way. In fact, the compiler will completely ignore it.
+}
